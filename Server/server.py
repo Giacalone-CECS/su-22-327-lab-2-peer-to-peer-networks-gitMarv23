7b279096b58525e20ac1ad165951f89e3c26214a
gitMarv23 <46500443+gitMarv23@users.noreply.github.com>
2022-07-30 18:22:01 -0700

    client-server connections troubleshooting

--- Server/server.py
+++ Server/server.py
@@ -2,11 +2,10 @@
 
 import socket
 
-HOST = ""  # Standard loopback interface address (localhost)
+HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-
     s.bind((HOST, PORT))
     s.listen()
     conn, addr = s.accept()
@@ -17,3 +16,5 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             if not data:
                 break
             conn.sendall(data)
+
+print("server listening")
