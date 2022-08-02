412ef922be748e2f4ab8cc6f4cdafcb73da2cbbc
BenjaminGreat5 <dikachiokenwa@gmail.com>
2022-08-02 16:15:37 -0700

    client.py

--- client/client.py
+++ client/client.py
@@ -6,7 +6,7 @@ HOST = socket.gethostbyname("server") # The server's hostname or IP address
 PORT = 65432  # The port used by the server
 
 print(f"Server IP address {HOST}")
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: #TCP Socket
     s.connect((HOST, PORT))
     s.sendall(b"Hello, world")
     data = s.recv(1024)
