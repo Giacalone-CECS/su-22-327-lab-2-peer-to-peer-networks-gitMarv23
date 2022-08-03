4ed998c7d0c735fb139944150066e8e15dbe20f4
gitMarv23 <46500443+gitMarv23@users.noreply.github.com>
2022-08-02 18:29:23 -0700

    container script launch success

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
+import subprocess # python subprocess module to run containers
+
 # prompt for number of desired nodes in the network
 print("How many nodes do you want?")
 nodes = int(input())
 
-for i in range(1, nodes):
+for i in range(1, (nodes + 1)):
     # run host command to launch multiple nodes
-    print("loop iteration success")
+    subprocess.run(["docker", "run", "-it", "su-22-327-lab-2-peer-to-peer-networks-gitmarv23_node"])
+    
