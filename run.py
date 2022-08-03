57d5a27545091fadff2be60bda7761b0298a1be8
BenjaminGreat5 <dikachiokenwa@gmail.com>
2022-08-03 14:23:58 -0700

    update dockerfile

--- run.py
+++ run.py
@@ -4,4 +4,8 @@ nodes = int(input())
 
 for i in range(1, nodes):
     # run host command to launch multiple nodes
+<<<<<<< Updated upstream
     print("loop iteration success")
+=======
+    subproceses.Popen(["docker", "run", "-it", "su-22-327-lab-2-peer-to-peer-networks-gitmarv23_node"])
+>>>>>>> Stashed changes
