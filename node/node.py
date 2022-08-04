d6f430d4a430b164df20b158644af1cabd6198f6
BenjaminGreat5 <dikachiokenwa@gmail.com>
2022-08-04 00:55:31 -0700

    socket building 50%

--- node/node.py
+++ node/node.py
@@ -6,6 +6,7 @@ PORT = "80" # desired socket port from docker compose
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create a new socket
 s.bind() # bind to the socket (currently in progress)
  # listen for any connections
+ #add a print statement
  # accept connection request
  # receive/send loop until communication closure
 s.close() # close socket connection
