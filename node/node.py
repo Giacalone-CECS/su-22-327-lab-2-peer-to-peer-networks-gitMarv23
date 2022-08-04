2ec6c114560bbdddb6d0f85ac913b00191735d95
gitMarv23 <46500443+gitMarv23@users.noreply.github.com>
2022-08-03 19:36:36 -0700

    testing py script auto launch

--- node/node.py
+++ node/node.py
@@ -1,11 +1,11 @@
 import socket
 
-HOST = "" # ip address to run through eth0 port to docker
-PORT = "80" # desired socket port from docker compose
+HOST = '127.0.0.1' # ip address to run through eth0 port to docker
+PORT = 80 # desired socket port from docker compose
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # create a new socket
-s.bind() # bind to the socket (currently in progress)
- # listen for any connections
- # accept connection request
- # receive/send loop until communication closure
-s.close() # close socket connection
+# create a new socket using 'with' to avoid having to include 'close()'
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT)) # bind to the socket (currently in progress)
+    s.listen() # listen for any connections
+     # accept connection request
+     # receive/send loop until communication closure
