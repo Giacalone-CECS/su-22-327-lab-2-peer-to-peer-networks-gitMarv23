7fd4755d351900bbcb44d0418349cade58a8b128
BenjaminGreat5 <dikachiokenwa@gmail.com>
2022-08-04 22:45:04 -0700

    client and server in one node

--- node/node.py
+++ node/node.py
@@ -1,12 +1,44 @@
 import socket
+from threading import Thread
+import time
 
-HOST = '127.0.0.1' # ip address to run through eth0 port to docker
-PORT = 80 # desired socket port from docker compose
+def server():
+    HOST = '127.0.0.1' # ip address to run through eth0 port to docker
+    PORT = 80 # desired socket port from docker compose
 
-# create a new socket using 'with' to avoid having to include 'close()'
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT)) # bind to the socket (currently in progress)
-    print('listening for connections')
-    s.listen() # listen for any connections
-     # accept connection request
-     # receive/send loop until communication closure
+    # create a new socket using 'with' to avoid having to include 'close()'
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((HOST, PORT)) # bind to the socket (currently in progress)
+        print('listening for connections')
+        s.listen() # listen for any connections
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            while True:
+                data = conn.recv(1024)
+                if not data:
+                    break
+                conn.sendall(data)
+                # accept connection request
+         # receive/send loop until communication closure
+
+
+
+
+def client ():
+    Host = "127.0.0.1"
+    Port = 80 #The port that the server uses
+
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((Host, Port))
+        s.sendall(f"Hello, world")
+        data = s.recv(1024)
+
+    print(f"Received {data!r}")
+
+Thread(target = server).start()
+time.sleep(0.5)
+Thread(target = client).start()
+
+
+
--- run.py
+++ run.py
@@ -9,11 +9,11 @@ f = open('./docker-compose.yml', mode='a')
 
 # edit docker-compose.yml to make node count scalable
 for i in range(1, (totalNodes + 1)):
-    print(' node' + str(nodeNum) + ':', file=f) # node service declaration
-    print('  build: ./node', file=f) # Dockerfile location for build
-    print('  command: python3 ./node.py', file=f) # command on docker compose container launch
-    print('  ports:', file=f) # declare ports
-    print('   - \"' + str(hostPort) + ':80\"', file=f)
+    print(' node' + str(nodeNum) + ':', file = f) # node service declaration
+    print('  build: ./node', file = f) # Dockerfile location for build
+    print('  command: python3 ./node.py', file = f) # command on docker compose container launch
+    print('  ports:', file = f) # declare ports
+    print('   - \"' + str(hostPort) + ':80\"', file = f)
     nodeNum += 1 # increment
     hostPort += 1 #increment host port for availability
 f.close() # close the file
@@ -23,5 +23,5 @@ subprocess.run(["docker", "compose", "up"])
 
 # clean up docker-compose.yml
 f = open('./docker-compose.yml', mode='w') # open file for cleanup
-print('version: \"3.9\"\nservices:', file=f) # prtint standard lines onto file
+print('version: \"3.9\"\nservices:', file = f) # print standard lines onto file
 f.close() # close file and terminate system
