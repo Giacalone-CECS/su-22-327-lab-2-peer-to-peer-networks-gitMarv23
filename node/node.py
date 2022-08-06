bf912fe9c29e23cfafe507c85cb7d019d997a0b1
BenjaminGreat5 <dikachiokenwa@gmail.com>
2022-08-05 22:10:38 -0700

    sent message in byte form

--- node/node.py
+++ node/node.py
@@ -1,6 +1,18 @@
 import socket
 from threading import Thread
 import time
+from ping3 import ping
+#scanning for ip address
+
+import os
+hostname = "127.0.0.1"
+response = os.system("ping -c 1 " + hostname)
+
+#and then check the response...
+if response == 0:
+    print (hostname, 'is up!')
+else:
+    print (hostname, 'is down!')
 
 def server():
     HOST = '127.0.0.1' # ip address to run through eth0 port to docker
@@ -8,19 +20,20 @@ def server():
 
     # create a new socket using 'with' to avoid having to include 'close()'
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST, PORT)) # bind to the socket (currently in progress)
+        s.bind((HOST, PORT)) # bind to the socket
         print('listening for connections')
         s.listen() # listen for any connections
-        conn, addr = s.accept()
+        conn, addr = s.accept() # accept connection request
         with conn:
             print(f"Connected by {addr}")
+            # receive/send loop until communication closure
             while True:
                 data = conn.recv(1024)
                 if not data:
                     break
                 conn.sendall(data)
-                # accept connection request
-         # receive/send loop until communication closure
+
+
 
 
 
@@ -31,13 +44,15 @@ def client ():
 
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((Host, Port))
-        s.sendall(f"Hello, world")
+        s.sendall(b"Hello, world")
         data = s.recv(1024)
 
     print(f"Received {data!r}")
 
 Thread(target = server).start()
+
 time.sleep(0.5)
+
 Thread(target = client).start()
 
 
