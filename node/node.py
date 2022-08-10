4fc92b43f84a2ec7c838dd419c4a41c7ca00207a
gitMarv23 <46500443+gitMarv23@users.noreply.github.com>
2022-08-10 01:34:47 -0700

    port scanning almost there

--- node/node.py
+++ node/node.py
@@ -6,28 +6,26 @@ from ping3 import ping
 HOST_SRV = socket.gethostname()             # ip address to run through eth0 port to docker
 HOST_CLI = socket.gethostbyname(HOST_SRV)   # get host name of other machines on the network
 PORT = 80                                   # desired socket port for container
-PORT_START = 30000                          # start port number for scanning
+PORT_START = 20000                          # start port number for scanning
 PORT_END = 60000                            # end number for port scanning
+ip_address = []                             # list of available ips
 
 # get total number of nodes in our system
 f = open('./totalNodes.txt', mode='r')  # open corresponding file
 totalNodes = int(f.read())              # pull total number of nodes
 f.close()                               # close file to prevent leak
 
-ip_address = []
+# pings for an ip on the network and appends the list if found
 def myping():
     for x in range(1, (totalNodes+1)):
         ipaddress =  "172.18.0." + str(x)
         if ping(ipaddress):
-            ip_address.append(ipaddress)
-            port_scan(ipaddress, PORT)
-        else:
-            return False
+            ip_address.append(ipaddress) # add ip to the list
 
-def server(HOST_SRV, PORT):
+def server(host, port):
     # create a new socket using 'with' to avoid having to include 'close()'
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST_SRV, PORT))            # bind to the socket
+        s.bind((host, port))            # bind to the socket
         print('listening for connections')
         s.listen()                          # listen for any connections
         conn, addr = s.accept()             # accept connection request
@@ -40,40 +38,36 @@ def server(HOST_SRV, PORT):
                 conn.sendall(data)
 
 # client definition for receiving and sending data
-def client(HOST_CLI, PORT):
+def client(client, port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((HOST_CLI, PORT))
+        s.connect((client, port))
         s.sendall(b"Hello, world") # sending message in bytes
         data = s.recv(1024)
     print(f"Received {data!r}")
-
-Thread(target = server, args=(HOST_SRV, PORT)).start()
-
-time.sleep(0.5)
-
-Thread(target = client, args=(HOST_CLI, PORT)).start()
 
 # port scanning the network for available servers
 def port_scan(ip, port):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   # new socket for scanning
     try:
-        s.connect((ip, port))                         # if socket connets return true
-        return True
+        s.connect((ip, port))                         
+        print('connection established')                     # if socket connets print message
     except:
-        return False
+        print('no connection established')
+
+Thread(target = server, args=(HOST_SRV, PORT)).start() # start server thread
+time.sleep(0.5)                                        # give a little break
+Thread(target = client, args=(HOST_CLI, PORT)).start() # start client thread
+
 
 start = time.time() # start calculating run time
 
 myping() # ip address assignment
-for i in ip_address:
-    print("ip address: " + i)
 
 # dictate port scanning based on the normal output from docker compose (ports 30,000 -> 60,000)
-for i in range(PORT_START, (PORT_END + 1)):
-    if port_scan(ip_address[i-PORT_START], i):
-        print(f'port {i} is open') # if found print on system
-    if len(ip_address) > totalNodes:
-        break
+for i in range(0, len(ip_address)):             # scan through our list of ip addresses
+    for j in range(PORT_START, (PORT_END + 1)):
+        if port_scan(ip_address[i], j):         # scan ports at current ip address
+            print(f'port {j} is open')          # if open print on system
 
 end = time.time()                       # end run time calculation
 print(f'Elapse Time: {end-start:.2f}s') # print the elapsed time for our system
