6013cb0768c27a4eff160e772a13ebeb29fd5dd8
gitMarv23 <46500443+gitMarv23@users.noreply.github.com>
2022-08-09 23:18:05 -0700

    total nodes shared through file

--- node/node.py
+++ node/node.py
@@ -3,13 +3,16 @@ from threading import Thread
 import time
 from ping3 import ping
 
-HOST_SRV = socket.gethostname() # ip address to run through eth0 port to docker
-HOST_CLI = socket.gethostbyname(HOST_SRV) # get host name of other machines on the network
-PORT = 80 # desired socket port for container
-PORT_START = 30000 # start port number for scanning
-PORT_END = 60000 # end number for port scanning
+HOST_SRV = socket.gethostname()             # ip address to run through eth0 port to docker
+HOST_CLI = socket.gethostbyname(HOST_SRV)   # get host name of other machines on the network
+PORT = 80                                   # desired socket port for container
+PORT_START = 30000                          # start port number for scanning
+PORT_END = 60000                            # end number for port scanning
 
 # get total number of nodes in our system
+f = open('./totalNodes.txt', mode='r')
+totalNodes = int(f.read())       # pull total number of nodes
+f.close()
 
 ip_address = []
 def ping():
@@ -23,10 +26,10 @@ def ping():
 def server(HOST_SRV, PORT):
     # create a new socket using 'with' to avoid having to include 'close()'
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((HOST_SRV, PORT)) # bind to the socket
+        s.bind((HOST_SRV, PORT))            # bind to the socket
         print('listening for connections')
-        s.listen() # listen for any connections
-        conn, addr = s.accept() # accept connection request
+        s.listen()                          # listen for any connections
+        conn, addr = s.accept()             # accept connection request
         with conn:
             print(f"Connected by {addr}")
             while True:
@@ -51,9 +54,9 @@ Thread(target = client, args=(HOST_CLI, PORT)).start()
 
 # port scanning the network for available servers
 def port_scan(port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # new socket for scanning
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   # new socket for scanning
     try:
-        s.connect((HOST_SRV, PORT)) # if socket connets return true
+        s.connect((HOST_SRV, PORT))                         # if socket connets return true
         return True
     except:
         return False
@@ -65,5 +68,5 @@ for port in range(PORT_START, (PORT_END + 1)):
     if port_scan(port):
         print(f'port {port} is open') # if found print on system
 
-end = time.time() # end run time calculation
+end = time.time()                       # end run time calculation
 print(f'Elapse Time: {end-start:.2f}s') # print the elapsed time for our system
