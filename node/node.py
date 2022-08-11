bad5bffbcb92ecd704dd98383f82544a876b5961
gitMarv23 <46500443+gitMarv23@users.noreply.github.com>
2022-08-10 21:22:57 -0700

    port scanner done

--- node/node.py
+++ node/node.py
@@ -18,7 +18,7 @@ f.close()                               # close file to prevent leak
 # pings for an ip on the network and appends the list if found
 def myping():
     for x in range(1, (totalNodes+1)):
-        ipaddress =  "172.18.0." + str(x)
+        ipaddress =  "172.20.0." + str(x+1)
         if ping(ipaddress):
             ip_address.append(ipaddress) # add ip to the list
 
@@ -51,19 +51,20 @@ def port_scan(ip, port):
         s.connect((ip, port))
         print('connection established')                           # if socket connets print message
 
+start = time.time() # start calculating run time
+
 Thread(target = server, args=(HOST_SRV, PORT)).start() # start server thread
 time.sleep(0.5)                                        # give a little break
 Thread(target = client, args=(HOST_CLI, PORT)).start() # start client thread
 
-start = time.time() # start calculating run time
-
 myping() # ip address assignment
-'''
+
 # dictate port scanning based on the normal output from docker compose (ports 30,000 -> 60,000)
-for i in range(0, len(ip_address)):             # scan through our list of ip addresses
+for i in range(0, len(ip_address)):                                  # scan through our list of ip addresses
     for j in range(PORT_START, (PORT_END + 1)):
-        if port_scan(ip_address[i], j):         # scan ports at current ip address
-            print(f'port {j} is open')          # if open print on system
-'''
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: # create new socket
+            result = s.connect_ex((ip_address[i], j))                # error return if found
+            if result == 0: print(f"Port {j} is open")
+
 end = time.time()                       # end run time calculation
 print(f'Elapse Time: {end-start:.2f}s') # print the elapsed time for our system
